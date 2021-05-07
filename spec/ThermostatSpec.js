56f479c4289472ea3f3caba846b87dd53f9420a6
Phil Smithies <philsmithies@gmail.com>
2021-05-07 12:25:57 +0200

    refactoring / removal magic numbers

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -20,23 +20,27 @@ describe('Thermostat', function() {
       expect(thermostat.tempDown()).toEqual(defaultTemperature - 10);
     });
     it ('Cannot lower the temperature lower than 10 degrees', () => {
-      thermostat = new Thermostat(10)
+      thermostat.temperature = 10
       expect(() => { thermostat.tempDown(); }).toThrowError('Cannot lower the temperature below 10 degrees');
     });
+    it("Can return it's current temperature", () => {
+      thermostat.temperature = 25
+      expect(thermostat.getCurrentTemperature()).toEqual(25)
+    });
   });
+  
   describe('Power saving mode', () => {
     it('Has a max temperature of 25 degrees when on', () =>{
-      thermostat.powerSavingMode(true);
+      thermostat.powerSavingModeOn();
       expect(thermostat.maxTemperature).toEqual(25);
     });
     it('It returns its max temperature of 25 degrees when turned off', () =>{
-      thermostat.powerSavingMode(true);
-      thermostat.powerSavingMode(false);
+      thermostat.powerSavingModeOn();
+      thermostat.powerSavingModeOff();
       expect(thermostat.maxTemperature).toEqual(32);
     });
     it('Has power saving mode turned on by default', () => {
-      thermostat.powerSavingMode();
-      expect(thermostat.maxTemperature).toEqual(25);
+      expect(thermostat.maxTemperature).toEqual(thermostat.TEMP_PSM_ON);
     });
   });
 
@@ -45,6 +49,21 @@ describe('Thermostat', function() {
       thermostat.tempUp();
       thermostat.reset();
       expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('current usage', () => {
+    it('Can display low usage for lower temparatures', () => {
+      thermostat.temperature = 10;
+      expect(thermostat.currentUsage()).toEqual('low-usage');
+    });
+    it('Can display medium usage for a medium temperature', () => {
+      thermostat.temperature = 24
+      expect(thermostat.currentUsage()).toEqual('medium-usage');
+    });
+    it('Can display high usage for a high temperature', () => {
+      thermostat.temperature = 32
+      expect(thermostat.currentUsage()).toEqual('high-usage');
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,30 +1,48 @@
 class Thermostat {
 
-  constructor(temperature = 20){
-    this.temperature = temperature;
-    this.maxTemperature = 32
+  constructor(){
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.TEMP_PSM_ON = 25;
+    this.TEMP_PSM_OFF = 32;
+    this.maxTemperature = this.TEMP_PSM_ON;
   }  
+
+  getCurrentTemperature(){
+    return this.temperature;
+  }
 
   tempUp() {
     return this.temperature += 10;
   }
 
   tempDown() {
-    if (this.temperature <= 10) {
+    if (this.temperature <= this.MINIMUM_TEMPERATURE) {
       throw new Error ('Cannot lower the temperature below 10 degrees');
     }
     return this.temperature -= 10;
   }
 
-  powerSavingMode(value = true){
-    if (value == true){
-      this.maxTemperature = 25;
-    } else {
-      this.maxTemperature = 32;
-    }
+  powerSavingModeOn(){
+    this.maxTemperature = this.TEMP_PSM_ON;
+  };
+
+  powerSavingModeOff(){
+    this.maxTemperature = this.TEMP_PSM_OFF;
   };
 
   reset() {
     this.temperature = 20;
+  }
+
+  currentUsage() {
+    if (this.temperature < 18){
+      return 'low-usage';
+    } else if (this.temperature <= 25) {
+      return 'medium-usage';
+    } else {
+      return 'high-usage';
+    }
   }
 };

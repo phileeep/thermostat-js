2f3aae247f1216375877d7b9b85b6f14a3bec180
Phil Smithies <philsmithies@gmail.com>
2021-05-07 13:26:36 +0200

    dom manipulation

--- src/Index.js
+++ src/Index.js
@@ -1,3 +1,36 @@
-document.addEventListener('tempUp', function(click){
-  console.log('This was clicked')
-})
+document.addEventListener("DOMContentLoaded", () => {
+
+  const updateTemperature = () => {
+    document.getElementById("current_temp").innerText = thermostat.temperature;
+  }
+
+  const thermostat = new Thermostat();
+  updateTemperature();
+
+  document.getElementById("tempUp").addEventListener("click", () => {
+    thermostat.tempUp();
+    updateTemperature();
+  });
+
+  document.getElementById("tempDown").addEventListener("click", () => {
+    thermostat.tempDown();
+    updateTemperature();
+  });
+
+  document.getElementById("resetTemp").addEventListener("click", () => {
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  document.getElementById("psm_on_button").addEventListener("click", () => {
+    thermostat.powerSavingModeOn();
+    document.getElementById("psm_status").innerHTML="<span class='psm_on'> on</span>";
+  });
+
+  document.getElementById("psm_off_button").addEventListener("click", () => {
+    thermostat.powerSavingModeOff();
+    document.getElementById("psm_status").innerHTML="<span class='psm_off'> off</span>";
+  });
+
+});
+
